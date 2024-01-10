50fccee7d1ef4c12d70164a9b5904528c65a4500
kk030902 <1957702689@qq.com>
2024-01-10 17:28:23 +0800

    origin

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Cargo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(45), nullable=False)
